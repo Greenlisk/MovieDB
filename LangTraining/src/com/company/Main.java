059230cef8766909d160a57b107b0867a133ccbb
Igor Serbai <iserbai@cogniance.com>
2016-01-29 20:15:37 +0200

    Data I/O added

--- LangTraining/src/com/company/Main.java
+++ LangTraining/src/com/company/Main.java
@@ -9,6 +9,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        processText("input_file.txt", "output_file.txt");
+        processData("input_file.data", "output_file.data");
+    }
+
+    private static void processText(String input_str, String output_str) {
         try (Scanner input =
                      new Scanner(new BufferedReader(new FileReader("input_file.txt")));
              PrintWriter output = new PrintWriter(new FileWriter("output_file.txt"))) {
@@ -42,6 +47,44 @@ public class Main {
         } finally {
             System.out.println("\nClosing streams...");
         }
+    }
 
+    private static void processData(String output_str) {
+        String[] notes = {"Stupid note",
+                "Silly note",
+                "Template note",
+                "Interesting note",
+                "Smart note"};
+        int[] budget = {10000, 8000, 7500, 4000, 2500};
+        boolean[] useful = {true, false, false, true, false};
+        try (DataInputStream input = new DataInputStream(
+                new BufferedInputStream(
+                        new FileInputStream(output_str)
+                ));
+             DataOutputStream output = new DataOutputStream(
+                     new BufferedOutputStream(
+                             new FileOutputStream(output_str)
+                     )
+             )) {
+            for (int i = 0; i < notes.length; ++i) {
+                output.writeBoolean(useful[i]);
+                output.writeInt(budget[i]);
+                output.writeUTF(notes[i]);
+            }
+            output.close();
+            System.out.println("Data Written");
+
+            while(true) {
+                boolean boo = input.readBoolean();
+                int money = input.readInt();
+                String note = input.readUTF();
+                System.out.println("- " + boo + " " + money + " " + note);
+            }
+        } catch (EOFException ex) {
+            System.err.println("Exceptional situation: " + ex.getMessage());
+        } catch (IOException ioex) {
+            System.err.println("Exceptional situation1: " + ioex.getMessage());
+        }
+        //No need of final due to usage of try-with-resources construction
     }
 }
